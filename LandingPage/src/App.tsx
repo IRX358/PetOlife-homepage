123eb6edb1e770e20f9c1e466e58a659eecbc3f9
irx358 <ir.358.786@gmail.com>
2026-05-10 09:38:50 +0530

    minor changes

--- LandingPage/src/App.tsx
+++ LandingPage/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar        from './components/Navbar';
 import Hero          from './components/Hero';
